923d3b7df423791bed4c73f9ebd47bcd1c1640c4
abdulsamigithub <asm640@gmail.com>
2020-07-06 04:35:05 +0500

    update react .2.2

--- src/App.js
+++ src/App.js
@@ -11,14 +11,15 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
+        
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React2.2
         </a>
-        <h1>Hellow</h1>
+        
       </header>
     </div>
   );
